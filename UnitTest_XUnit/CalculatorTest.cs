8b4ddc4cfc260d62deeff0636269aaa229747ef4
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-05-02 17:46:54 -0300

    Basic Test

---
+++ UnitTest_XUnit/CalculatorTest.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace UnitTest_XUnit
+{
+    public class CalculatorTest
+    {
+        private Calculator _alvoDoTeste = new Calculator();
+
+        [Fact]
+        public void SumOfTwoNumbersMustBeTheSameAsTheResult()
+        {
+            _alvoDoTeste.Add(5);
+            _alvoDoTeste.Add(4);
+
+            Assert.Equal(9, _alvoDoTeste.Value);
+        }
+
+        [Theory]
+        [InlineData(11, 5, 6)]
+        public void SumOfTwoNumbersMustBeTheSameAsTheResult_Theory(int expectedValue, int value1, int value2)
+        {
+            _alvoDoTeste.Add(value1);
+            _alvoDoTeste.Add(value2);
+
+            Assert.Equal(expectedValue, _alvoDoTeste.Value);
+        }
+
+        [Theory]
+        //[MemberData(nameof(DataForTestOfSum))]
+        [ClassData(typeof(ClassWithDataForTestOfSum))]
+        public void SumOfManyNumbersMustBeTheSameAsTheResult_Theory(int expectedValue, int[] values)
+        {
+            foreach (int value in values)
+            {
+                _alvoDoTeste.Add(value);
+            }
+
+            Assert.Equal(expectedValue, _alvoDoTeste.Value);
+        }
+
+        public static IEnumerable<object[]> DataForTestOfSum()
+        {
+            yield return new object[] { 13, new int[] { 5, 8 } };
+            yield return new object[] { 10, new int[] { 5, 2, 2, 1 } };
+            yield return new object[] { -10, new int[] { 10, -20 } };
+            yield return new object[] { 6, new int[] { 3, 3 } };
+        }
+
+        public class ClassWithDataForTestOfSum : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { 13, new int[] { 5, 8 } };
+                yield return new object[] { 10, new int[] { 5, 2, 2, 1 } };
+                yield return new object[] { -10, new int[] { 10, -20 } };
+                yield return new object[] { 6, new int[] { 3, 3 } };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ UnitTest_XUnit/GuidGeneratorTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTest_XUnit
+{
+    [CollectionDefinition("guid collection", DisableParallelization = false)]
+    public class GuidGeneratorCollectionDefinition: ICollectionFixture<GuidGenerator> { }
+
+    //public class GuidGeneratorTest : IClassFixture<GuidGenerator>
+    [Collection("guid collection")]
+    public class GuidGeneratorTest
+    {
+        private readonly GuidGenerator _generator;
+        private readonly ITestOutputHelper _output;
+
+        public GuidGeneratorTest(ITestOutputHelper output, GuidGenerator generator)
+        {
+            _output = output;
+            _generator = generator;
+        }
+
+        [Fact]
+        public void TestOne()
+        {
+            var guid = _generator.RandomGuid;
+            _output.WriteLine($"O guid é {guid}");
+        }
+
+        [Fact]
+        public void TestTwo()
+        {
+            var guid = _generator.RandomGuid;
+            _output.WriteLine($"O guid é {guid}");
+        }
+    }
+
+    [Collection("guid collection")]
+    public class GuidGeneratorTestTwo
+    {
+        private readonly GuidGenerator _generator;
+        private readonly ITestOutputHelper _output;
+
+        public GuidGeneratorTestTwo(ITestOutputHelper output, GuidGenerator generator)
+        {
+            _output = output;
+            _generator = generator;
+        }
+
+        [Fact]
+        public void TestOne()
+        {
+            var guid = _generator.RandomGuid;
+            _output.WriteLine($"O guid é {guid}");
+        }
+
+        [Fact]
+        public void TestTwo()
+        {
+            var guid = _generator.RandomGuid;
+            _output.WriteLine($"O guid é {guid}");
+        }
+    }
+}
